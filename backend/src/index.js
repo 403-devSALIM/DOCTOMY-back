e761990f62eb352a34fc9b80bae77b7e520faab5
salimbdi <s_bdirina@estin.dz>
2025-09-15 12:45:01 +0200

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,23 @@ import express from "express" ;
 import "dotenv/config" ; 
 import authRouter from "./routes/authRoutes.js"
 import {connectdb} from "./lib/db.js"
+import bookRoute from "./routes/bookroute.js"
+import cors from "cors" ; 
+import job from "./lib/cron.js";
+
+job.start();
+
+
+
 const app = express();
 const PORT  = process.env.PORT
 
 app.use(express.json());
-
+app.use(cors());
 
 app.use("/api/auth" , authRouter)
+
+app.use("/api/book" , bookRoute)
 app.listen(PORT , ()=>{
     console.log(`server is runnig on port ${PORT}`);
     connectdb();
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const connectdb = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Database connected successfuly: ${conn.connection.host}`);
+    console.log(`Database connected successfull: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Error connecting to DB:", error.message);
+    console.error("Error connecting to Database:", error.message);
     process.exit(1); // Exit process with failure
   }
 };
